b11bd0547582959ecec37c55d13aa443625cc6ca
Elton Santos <eltonaxl@hotmail.com>
2025-01-15 00:01:53 -0300

    Add login with Google

--- src/app/components/LoginForm.tsx
+++ src/app/components/LoginForm.tsx
@@ -2,6 +2,7 @@
 
 import { signIn } from "next-auth/react"
 import { useSearchParams } from "next/navigation"
+import LoginGoogle from "./LoginGoogle"
 
 export default function LoginForm() {
   const searchParams = useSearchParams()
@@ -25,11 +26,12 @@ export default function LoginForm() {
   }
 
   return (
-    <form onSubmit={login} className="bg-white p-12 rounded-lg w-96 max-w-full flex justify-center items-center flex-col gap-2">
+    <form onSubmit={login} className="flex flex-col gap-2 w-full justify-center items-center">
       <h2 className="font-bold text-lg mb-3 text-slate-600">Faça seu login</h2>
       <input type="email" name="email" placeholder="Digite seu email" className="input input-primary w-full bg-white" />
       <input type="password" name="password" placeholder="Digite seu password" className="input input-primary w-full bg-white" />
       <button type="submit" className="btn btn-primary w-full text-white">Login</button>
+
       {error === "CredentialsSignin" && (
         <div className="text-red-500">Erro no login</div>
       )}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import LoginForm from "./components/LoginForm";
+import LoginGoogle from "./components/LoginGoogle";
 
 export default function Home() {
   console.log("oi home")
   return (
     <main>
       <div className="flex justify-center items-center h-screen bg-slate-600 px-5">
-        <LoginForm />
+        <div className="bg-white p-12 rounded-lg w-96 max-w-full flex justify-center items-center flex-col gap-2">
+          <LoginForm />
+          <LoginGoogle />
+        </div>
       </div>
     </main>
   );
